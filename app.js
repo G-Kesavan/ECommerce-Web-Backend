55250216d0ed2f6bddee74e692a46e0a8f413472
G-Kesavan <mr.kesavaperumal@gmail.com>
2025-11-21 23:04:29 +0530

    retry-2

--- app.js
+++ app.js
@@ -9,10 +9,22 @@ const catchError = require("./Middleware/catchError");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "https://project-emarket.web.app");
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type,Authorization");
+
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200); // important!
+  }
+
+  next();
+});
+
 //MIDDLE WARES
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors());
 app.use(
   cors({
     origin: "https://project-emarket.web.app",
