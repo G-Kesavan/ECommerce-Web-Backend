e7f4033cac90d9fe0ff2f98fe34e0a8a6573a772
G-Kesavan <mr.kesavaperumal@gmail.com>
2025-11-21 23:18:17 +0530

    retry-4

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ const cookieParser = require("cookie-parser");
 const cors = require("cors");
 
 //MIDDLE WARES
+
+app.set("trust proxy", 1); 
 app.use(express.json());
 app.use(cookieParser());
 app.use(
